0a624edda3a79ac5c10bf8ec9b702ad6e401ccbe
Samantha An <an.samantha2007@gmail.com>
2026-09-11 12:33:58 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = "".join([char for char in s if char.isalnum()]).lower()
+        return self.checkPalindrome(new_s)
+
+
+    def checkPalindrome(self,new_s:str) -> bool:
+        #base case
+        if len(new_s) == 1 or len(new_s) == 0:
+            return True
+        #recursive case
+        elif new_s[0] == new_s[-1]:
+            results = True
+        else:
+            results= False
+        results = self.isPalindrome(new_s[1:-1]) & results
+
+        return results
